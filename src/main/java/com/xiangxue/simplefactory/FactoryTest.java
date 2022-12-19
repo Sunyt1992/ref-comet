62447b3190666832dfc767ce28310127070d1681
Sunyt1992 <1014898807@qq.com>
2022-12-19 17:22:53 +0800

    sdfsdf

---
+++ src/main/java/com/xiangxue/simplefactory/FactoryTest.java
@@ -0,0 +1,13 @@
+package com.xiangxue.simplefactory;
+
+/**
+ * @projectName: ref-comet
+ * @package: com.xiangxue.simplefactory
+ * @className: FactoryTest
+ * @author: sunyingtao
+ * @description: TODO
+ * @date: 2022/12/19 17:22
+ * @version: 1.0
+ */
+public class FactoryTest {
+}
